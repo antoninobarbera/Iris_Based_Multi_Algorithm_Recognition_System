324cfd339bbc4be7e551e8d3c8536791cf6ce2bd
AntoninoBarbera <antonino.barbera001@unikorestudent.it>
2025-01-23 17:36:22 +0100

    main e data_classes

---
+++ data_classes/manage_dataset.py
@@ -0,0 +1,90 @@
+import os
+import pickle
+import cv2 as cv
+
+
+class CASIA_dataset():
+    
+    __slots__ = ['config', 'rec_test', 'rec_tot', 'data_rec']
+    
+    def __init__(self, config):
+        self.config = config
+        self.rec_test = None
+        self.rec_tot = None
+        self.data_rec = None
+    
+    def load_dataset(self):
+        dataset_path = 'dataset'
+        casia_path = os.path.join(dataset_path, 'CASIA.pkl')
+        
+        if not os.path.exists(dataset_path):
+            print(f"Error: The folder '{dataset_path}' does not exist.")
+            return
+
+        if not os.path.isfile(casia_path):
+            print(f"Error: The file '{casia_path}' does not exist.")
+            return
+        
+        casia_path = os.path.join('dataset', 'CASIA.pkl')
+        
+        try:
+            with open(casia_path, 'rb') as file:
+                casia = pickle.load(file)
+        except Exception as e:
+            print(f"Error: Failed to open or load the file '{casia_path}'.")
+            print(f"Exception details: {str(e)}")
+            return
+
+        rec_1 = casia['rec_1']
+        rec_2 = casia['rec_2']
+        rec_3 = casia['rec_3']
+        rec_4 = casia['rec_4']
+        rec_5 = casia['rec_5']
+        rec_6 = casia['rec_6']
+        rec_7 = casia['rec_7']
+
+        self.rec_test = [rec_3, rec_4, rec_7]
+        self.rec_tot = [rec_3, rec_4, rec_7, rec_1, rec_2, rec_5, rec_6]
+        self.data_rec = [rec_1, rec_2, rec_5, rec_6]
+        
+        return self.rec_test, self.rec_tot, self.data_rec
+        
+    def get_rec_test(self):
+        return self.rec_test
+    
+    def get_rec_tot(self):
+        return self.rec_tot
+        
+    def get_data_rec(self):
+        return self.data_rec
+    
+    
+class Manage_file():
+    
+    def __init__(self):
+        self.trasformed_path = 'trasformed_irises'
+        self.segmented_path = 'segmented_irises'
+        self.keypoints_path = 'keypoints_irises'
+        
+    def create_folder_image(self):
+        os.makedirs(self.trasformed_path, exist_ok=True)
+        segmented_path = os.path.join(self.trasformed_path, self.segmented_path)
+        keypoints_path = os.path.join(self.trasformed_path, self.keypoints_path)
+        os.makedirs(segmented_path, exist_ok=True)
+        os.makedirs(keypoints_path, exist_ok=True)
+        
+        return segmented_path, keypoints_path
+
+    
+    def save_image(self, segmented_images, keypoints_images):
+
+        print("    Saving segmented images...")
+        for image, path in segmented_images:
+            cv.imwrite(path, image)
+            
+        print("    Saving keypoints images...")
+        for image, path in keypoints_images:
+            cv.imwrite(path, image)
+
+        print("    Images saved successfully!\n")
+    
--- main.py
+++ main.py
@@ -3,42 +3,52 @@ import pickle
 import cv2 as cv
 from iris import iris_class
 from tools.file_manager import configuration
+from data_classes.manage_dataset import CASIA_dataset
+from data_classes.manage_dataset import Manage_file
 from identification import id_class
 import warnings
-warnings.filterwarnings('ignore')
+import gc
+import time
 
-if  __name__ == '__main__':  
-    config = configuration()
-    with open('CASIA.pkl', 'rb') as file:
-       casia = pickle.load(file)
+warnings.filterwarnings("ignore")
 
-    rec_1 = casia['rec_1']
-    rec_2 = casia['rec_2']
-    rec_3 = casia['rec_3']
-    rec_4 = casia['rec_4']
-    rec_5 = casia['rec_5']
-    rec_6 = casia['rec_6']
-    rec_7 = casia['rec_7']
+if  __name__ == '__main__':
+   
+   start_time = time.time()
+   
+   config = configuration()
+   
+   casia_dataset = CASIA_dataset(config)
+   rec_test, rec_tot, data_rec = casia_dataset.load_dataset()
+   
+   manage_file = Manage_file()
+   segmented_path, keypoints_path = manage_file.create_folder_image()
+   
+   print('\nLoading images...')
+   print('\nSegmentation - Normalization - Feature extraction in progress...\n')
 
-    
-    rec_test = [rec_3, rec_4, rec_7]
-    irises = []
-
-    print('Caricamento dati')
-
-    for rec in rec_test:
+   segmented_images = []
+   keypoints_images = []
+   irises = []
+   
+   print('Operation in test image...')
+   for rec in rec_test:
       for i in range (0, 100):
          eye = rec[i]
          iris_obtained = iris_class(eye, i, config)
          iris_obtained.segmentation()
+         segmented_image = iris_obtained.get_segmented_image()
+         segmented_images.append((segmented_image, os.path.join(segmented_path, f'{i}.jpeg')))
          iris_obtained.feature_extraction()
-         new_image = iris_obtained.get_keypoints_image()
+         keypoints_image = iris_obtained.get_keypoints_image()
          iris_obtained.set_iris_code()
          irises.append(iris_obtained)
-         
-
-    rec_tot = [rec_3, rec_4, rec_7, rec_1, rec_2, rec_5, rec_6]
-    for rec in rec_tot:
+         keypoints_images.append((keypoints_image, os.path.join(keypoints_path, f'{i}.jpeg')))
+   
+   manage_file.save_image(segmented_images, keypoints_images)
+   
+   print('Operation in total image...')
+   for rec in rec_tot:
       for i in range (100, 108):
          eye = rec[i]
          iris_obtained = iris_class(eye, i, config)
@@ -48,9 +58,9 @@ if  __name__ == '__main__':
          iris_obtained.set_iris_code()
          irises.append(iris_obtained)
 
-    data_rec = [rec_1, rec_2, rec_5, rec_6] 
-    data_dict = {i : [] for i in range(0, 108)}
-    for rec in data_rec:
+   print('Operation in train_image...')
+   data_dict = {i : [] for i in range(0, 108)}
+   for rec in data_rec:
       for i in range (0, 100):
          eye = rec[i]
          iris_obtained = iris_class(eye, i, config)
@@ -59,40 +69,42 @@ if  __name__ == '__main__':
          new_image = iris_obtained.get_keypoints_image()
          iris_obtained.set_iris_code()
          data_dict[i].append(iris_obtained)
-
-
-    
-    id = id_class(config, data_dict)
-    tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
-    tot = 0
-    for iris in irises:
-        tot += 1
-        flag, label = id.identification(iris)
-        if flag:
-           if iris.get_idx() == label:
-              tp += 1
-           else:
-              fp += 1
-        else:
+   print('fine') 
+         
+   id = id_class(config, data_dict)
+   tp, fp, tn, fn, tot = 0, 0, 0, 0, 0
+   
+   print('\nIdentification:')
+   for iris in irises:
+      tot += 1
+      flag, label = id.identification(iris)
+      if flag:
+         if iris.get_idx() == label:
+            tp += 1
+         else:
+            fp += 1
+      else:
             if iris.get_idx() < 100:
                fn += 1
             else:
                tn += 1
 
-    print('TP ' + str(tp))
-    print('FP ' + str(fp))
-    print('TN ' + str(tn))
-    print('FN ' + str(fn))
+   print('\tTP ' + str(tp))
+   print('\tFP ' + str(fp))
+   print('\tTN ' + str(tn))
+   print('\tFN ' + str(fn))
 
-    print('tot ' + str(tot))
-    accuracy = (tp + tn) / tot * 100
-    far = fp / (fp + tn) * 100
-    frr = fn / (fn + tp) * 100
-    
-    print('accuracy ' + str(round(accuracy, 2)) + " %")
-    print('FAR ' + str(round(far, 2)) + " %")
-    print('FRR ' + str(round(frr, 2)) + " %")
-           
+   print('\nPerformance achieved (' + str(tot) + ')')
+   accuracy = (tp + tn) / tot * 100
+   far = fp / (fp + tn) * 100
+   frr = fn / (fn + tp) * 100
+   
+   print('\taccuracy ' + str(round(accuracy, 2)) + " %")
+   print('\tFAR ' + str(round(far, 2)) + " %")
+   print('\tFRR ' + str(round(frr, 2)) + " %")
+   
+   end_time = time.time() 
+   execution_time = end_time - start_time
+   execution_time_minutes = execution_time / 60
+   print(f"\nTempo di esecuzione: {execution_time:.2f} minuti")
+   
